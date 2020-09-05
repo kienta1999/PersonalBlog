788c1d874a2679d51c88567cd632042b5781edcf
Kien Ta <talekien1710@gmail.com>
2020-09-05 10:00:13 +0700

    about and contact added

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ app.get('/', (req, res) => {
   res.render("home", {homeStartingContent: homeStartingContent});
 });
 
-
+app.get('/about', (req, res) => {
+  res.render("about", {aboutContent: aboutContent});
+});
+app.get('/contact', (req, res) => {
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 
